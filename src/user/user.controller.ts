0bffcd737b7656941f287b1c65e300ccda31577c
Eyob Simachew <eyob.ugr-1326-16@aau.edu.et>
2025-10-31 02:36:20 +0300

    Adding_documentation

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,15 +1,27 @@
-import { Controller, Get, Param, Delete, ParseIntPipe } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Param,
+  Delete,
+  ParseIntPipe,
+  UseGuards,
+} from '@nestjs/common';
 import { UserService } from './user.service';
+import { AuthGuard } from 'src/common/guard/auth.guard';
+import { RoleGuard } from 'src/common/guard/role.guard';
+import { Role } from 'src/common/decorators/role.decorator';
 @Controller('user')
+@UseGuards(AuthGuard, RoleGuard)
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Get()
+  @Role(['ADMIN'])
   async findAll() {
     return await this.userService.findAll();
   }
-
   @Delete(':id')
+  @Role(['ADMIN'])
   async remove(@Param('id', ParseIntPipe) id: number) {
     return await this.userService.remove(id);
   }
