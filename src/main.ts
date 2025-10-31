feb05cef7a1cab69570d88102469a5442e69c1a0
Tin21habesha <168670435+Tin21habesha@users.noreply.github.com>
2025-10-31 10:52:48 +0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,11 @@ import { AppModule } from './app.module';
 import cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use(cookieParser());
   app.enableCors({
-    origin: ['http://localhost:5173','https://primetrade-ai-frontend.vercel.app/'],
+    origin: ['http://localhost:5173','https://primetrade-ai-frontend.vercel.app'],
     credentials: true,
   });
+  app.use(cookieParser());
   
 
   await app.listen(process.env.PORT ?? 9000);
